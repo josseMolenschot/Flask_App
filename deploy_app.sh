fae02721088dc197ddb97ae74bec5da3cc8574e9
josseMolenschot <josse.molenschot@student.odisee.be>
2024-12-04 14:12:08 +0000

    a

---
+++ deploy_app.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/josseMolenschot/Flask_App/flask-app/
+mkdir flask_env
+cd flask_env
+python3 -m venv flask_env
+source flask_env/bin/activate
+pip install flask
+python3 sample_app.py
